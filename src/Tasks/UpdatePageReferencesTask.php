ef9c3aea130beafbb8a5b025d7172a0316ed8bad
John Learn <jflearn@logicbrush.com>
2019-07-16 07:37:04 -0400

    add documentation.

--- src/Tasks/UpdatePageReferencesTask.php
+++ src/Tasks/UpdatePageReferencesTask.php
@@ -8,6 +8,9 @@ use SilverStripe\Dev\MigrationTask;
 
 class UpdatePageReferencesTask extends MigrationTask {
 
+    protected $title = "Update SiteMapPage DB References"; 
+    protected $description = "Updates the references to the class name in the datbase to the FQCN."; 
+
     public function up() {
         $this->runSql('SiteMapPage', SiteMapPage::class);
     }
