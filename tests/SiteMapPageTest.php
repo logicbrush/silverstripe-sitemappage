ccdb0baa3d58affb9d4b192f988c934f732dbaff
John Learn <jflearn@logicbrush.com>
2019-07-26 13:15:02 -0400

    I think I have something happenning...

--- tests/SiteMapPageTest.php
+++ tests/SiteMapPageTest.php
@@ -3,14 +3,23 @@
 namespace Logicbrush\SiteMapPage\Tests;
 
 use Logicbrush\SiteMapPage\Model\SiteMapPage;
-use SilverStripe\Dev\FunctionalTest;
+use SilverStripe\Dev\SapphireTest;
 
-class SiteMapPageTest extends FunctionalTest {
+class SiteMapPageTest extends SapphireTest {
 
-    public function testIt() {
-        $page = new SiteMapPage();
-        $page->write();
-        $this->assertTrue(false);
+    protected static $fixture_file = 'Pages.yml';
+
+    /**
+     * Check the structure of the sitemap generated from the example
+     * fixture file.
+     */
+    public function testGeneratingSiteMap() {
+        $page = $this->objFromFixture(SiteMapPage::class, 'sitemap');
+        $html = $page->getSiteMap();
+        $this->assertEquals(
+            '<ul><li><a href="/">Home</a></li><li><a href="/services/">Services</a><ul><li><a href="/services/service-1/">Service 1</a></li><li><a href="/services/service-2/">Service 2</a></li></ul></li><li><a href="/site-map/">Site Map</a></li></ul>',
+            $html->Value
+        );
     }
     
 }
