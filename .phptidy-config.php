f4777fef367ce733baf94087ab4aff9a6dd27500
John Learn <jflearn@logicbrush.com>
2023-05-19 15:40:02 -0400

    add tidy config

---
+++ .phptidy-config.php
@@ -0,0 +1,9 @@
+<?php
+
+$add_operator_space = true;
+$fix_round_bracket_space = true;
+$add_file_docblock = true;
+$add_function_docblocks = true;
+$add_doctags = true;
+$fix_docblock_format = true;
+$fix_docblock_space = true;
