bbbe6b821b6b3f662d9bcc5760eff799cefe532f
John Learn <jflearn@logicbrush.com>
2023-05-25 18:34:27 -0400

    show redirector pages

--- src/Model/SiteMapPage.php
+++ src/Model/SiteMapPage.php
@@ -11,7 +11,6 @@ namespace Logicbrush\SiteMapPage\Model;
 use Page;
 use PageController;
 use SilverStripe\Blog\Model\Blog;
-use SilverStripe\CMS\Model\RedirectorPage;
 use SilverStripe\ORM\FieldType\DBField;
 
 class SiteMapPage extends Page {
@@ -27,7 +26,7 @@ class SiteMapPage extends Page {
 	 * @return unknown
 	 */
 	public function getSiteMap() {
-		return $this->makeSiteMap( \Page::get()->filter( 'ParentID', 0 ) );
+		return $this->makeSiteMap( Page::get()->filter( 'ParentID', 0 ) );
 	}
 
 
@@ -42,7 +41,7 @@ class SiteMapPage extends Page {
 		$html = "";
 
 		foreach ( $pages as $page ) {
-			if ( ( $page->ShowInMenus || $page->ShowInSearch ) && ! ( $page instanceof RedirectorPage ) ) {
+			if ( $page->ShowInMenus || $page->ShowInSearch ) {
 				$html .= "<li><a href=\"{$page->Link()}\">" . htmlspecialchars( $page->MenuTitle ) . '</a>';
 				if ( $page instanceof Blog ) {
 					// Sort blog entries by descending date.
