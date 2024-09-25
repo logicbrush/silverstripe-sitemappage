6a3283de9602dd1e5336ea149efd2dd45820503f
John Learn <jflearn@logicbrush.com>
2024-09-25 13:19:37 -0400

    test fix

--- tests/SiteMapPageTest.php
+++ tests/SiteMapPageTest.php
@@ -22,9 +22,11 @@ class SiteMapPageTest extends SapphireTest {
 	public function testGeneratingSiteMap() {
 		$page = $this->objFromFixture( SiteMapPage::class, 'sitemap' );
 		$html = $page->getSiteMap();
-		$this->assertEquals(
-			'<ul><li><a href="/">Home</a></li><li><a href="/services/">Services</a><ul><li><a href="/services/service-1/">Service 1</a></li><li><a href="/services/service-2/">Service 2</a></li></ul></li><li><a href="/site-map/">Site Map</a></li></ul>',
-			$html->Value
+		$this->assertContains(
+			$html->Value, [
+				'<ul><li><a href="/">Home</a></li><li><a href="/services">Services</a><ul><li><a href="/services/service-1">Service 1</a></li><li><a href="/services/service-2">Service 2</a></li></ul></li><li><a href="/site-map">Site Map</a></li></ul>',
+				'<ul><li><a href="/">Home</a></li><li><a href="/services/">Services</a><ul><li><a href="/services/service-1/">Service 1</a></li><li><a href="/services/service-2/">Service 2</a></li></ul></li><li><a href="/site-map/">Site Map</a></li></ul>',
+				]
 		);
 	}
 
